f70885c8fff56f34134d1a070db189e5ff0b8291
julixxx23 <jorellanao12@miumg.edu.gt>
2026-07-16 23:50:04 -0600

    Feat; comentary

--- src/main/java/com/microservices/orderservice/shared/domain/exception/NotFoundException.java
+++ src/main/java/com/microservices/orderservice/shared/domain/exception/NotFoundException.java
@@ -1,5 +1,5 @@
 package com.microservices.orderservice.shared.domain.exception;
-//Atrapa
+//Atrapa errores
 public class NotFoundException extends RuntimeException{
     protected NotFoundException(String message){
         super(message);
