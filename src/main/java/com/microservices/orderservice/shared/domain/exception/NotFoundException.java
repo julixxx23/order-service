56c8f3c4f9f6fce432f9b7ea117de10a13f788a5
julixxx23 <jorellanao12@miumg.edu.gt>
2026-07-23 23:37:12 -0600

    Comentarys

--- src/main/java/com/microservices/orderservice/shared/domain/exception/NotFoundException.java
+++ src/main/java/com/microservices/orderservice/shared/domain/exception/NotFoundException.java
@@ -1,5 +1,5 @@
 package com.microservices.orderservice.shared.domain.exception;
-//Atrapa errores del capa 8
+//Atrapa errores del capa 8 y se los hace ver
 public class NotFoundException extends RuntimeException{
     protected NotFoundException(String message){
         super(message);
